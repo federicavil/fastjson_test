85ea8303618c1eddb56e8d08f2c1b150cf9c0120
Federica <federica_vil@yahoo.it>
2022-04-14 09:01:42 +0200

    Initial commit

---
+++ src/test/java/TestList.java
@@ -0,0 +1,5 @@
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import junit.framework.TestCase;
+
+public class TestList {
+}
---
+++ src/test/java/TestListStringField_Stream.java
@@ -0,0 +1,5 @@
+import com.alibaba.fastjson.JSONReader;
+import junit.framework.TestCase;
+
+public class TestListStringField_Stream {
+}
